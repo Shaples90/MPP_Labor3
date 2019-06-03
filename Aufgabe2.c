7ceb8e5b7ba9dfc9024dbd6c5ce84a8f41df0b3f
Shaples90 <daejinseon@hotmail.com>
2019-06-03 17:55:09 +0200

    Laborversuch 3 Aufgabe 2

--- Aufgabe2.c
+++ Aufgabe2.c
@@ -1,18 +1,15 @@
-/*---------------------------------------------------------------------------------------
- * Testprogram:  ADC - Wägeverfahren
- * 	             Measure of input voltage
- *                                                                  Schlundt,Seon 05/2019
- *--------------------------------------------------------------------------------------x
-*/
+// Laborversuch 3 Aufgabe 2
 
 #include "tm4c1294ncpdt.h"
 #include "stdio.h"
+#include "stdint.h"
 
 void wait(unsigned long timevalue)
 {
 	int tmp = 0;
 	for(tmp=0; tmp < timevalue; tmp++);
 }
+
 void main(void)
 {
 	SYSCTL_RCGCGPIO_R |= ((1 << 3) | (1 << 9) | (1 << 10) | (1 << 11));			
@@ -27,20 +24,23 @@ void main(void)
 	GPIO_PORTL_DIR_R |= 0x07;
 	GPIO_PORTM_DIR_R |= 0xFF;
 
+	GPIO_PORTK_DATA_R &= ~0xFF;
+
 	while(1)
 	{
-		GPIO_PORTK_DATA_R &= ~0xFF;
 		unsigned short Kout = 0, bit = 0;
 
 		for(bit = 0x80; 0x01; bit = bit / 2)
 		{
 			Kout |= bit;
 			GPIO_PORTK_DATA_R |= Kout;
-			wait(500000);
+			wait(300);
 			if((GPIO_PORTD_AHB_DATA_R & 0x01) == 0x01)
 			{
 				Kout &= ~bit;
 			}
+			else
+				continue;
 		}
 		printf("%d\n", Kout);
 
