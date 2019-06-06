e292e42cb204957316370413ea986dcf3ba9c943
Shaples90 <daejinseon@hotmail.com>
2019-06-06 11:44:33 +0200

    Laborversuch 3 Aufgabe 2

--- Aufgabe2.c
+++ Aufgabe2.c
@@ -1,10 +1,4 @@
-/*Aufgabe 2:Wägeverfahren Suksessive Approximation
-
-* Mit Externen DAC wird ein Digitalvoltmeter eingerichtet
-
-*/
-
-// Autor: Nikolas Rieder
+// Laborversuch 3 Aufgabe 2
 
 #include <stdio.h>
 #include <stdint.h>
@@ -13,7 +7,6 @@
 void wait(unsigned long time)
 {
    unsigned long j;
-
    for (j = 0; j < time; j++);   // wait-loop
 }
 
@@ -22,77 +15,74 @@ void ConfigPorts(void)
     SYSCTL_RCGCGPIO_R |= ((1 << 3) | (1 << 10) | (1 << 11) | (1 << 9));            // clock enable port D,L,M
     while(!(SYSCTL_PRGPIO_R & ((1 << 3) | (1 << 10) | (1 << 11) | (1 << 9))));     // wait for port D,L,M clock
 
-    GPIO_PORTD_AHB_DEN_R |= 0x03;                                                  // PD(1:0) enable
-    GPIO_PORTL_DEN_R |= 0x07;                                                      // PD(2:0) enable
-    GPIO_PORTM_DEN_R |= 0xFF;                                                      // PM(7:0) enable
-    GPIO_PORTK_DEN_R |= 0xFF;                                                      // PM(7:0) enable
+    GPIO_PORTD_AHB_DEN_R |= 0x03;   // PD(1:0) enable
+    GPIO_PORTL_DEN_R |= 0x03;       // PL(1:0) enable
+    GPIO_PORTM_DEN_R |= 0xFF;       // PM(7:0) enable
+    GPIO_PORTK_DEN_R |= 0xFF;       // PK(7:0) enable
 
-    GPIO_PORTD_AHB_DIR_R |= 0x00;                                                  // PD(1:0) define input
-    GPIO_PORTM_DIR_R |= 0xFF;                                                      // PM(7:0) define output
-    GPIO_PORTL_DIR_R |= 0x07;                                                      // PM(2:0) define output
-    GPIO_PORTK_DIR_R |= 0xFF;                                                      // PM(7:0) define output
+    GPIO_PORTD_AHB_DIR_R |= 0x00;   // PD(1:0) define input
+    GPIO_PORTM_DIR_R |= 0xFF;       // PM(7:0) define output
+    GPIO_PORTL_DIR_R |= 0x03;       // PL(1:0) define output
+    GPIO_PORTK_DIR_R |= 0xFF;       // PK(7:0) define output
+
+    GPIO_PORTL_DATA_R |= 0x03;      // PL(1:0) output HIGH
 }
-
-/**
-
- * Erfassung der Spannung
-
- */
 
 unsigned char DACEXTERN(void)
 {
 
-	unsigned char VOLT = 0x80;
-	unsigned char BIT = 0x80;
+	unsigned char VOLT, BIT = 0x80;
 
-    while(BIT >= 0x01)                                                                            // PORTK wird zum Start auf MSB gesetzt. Bei jeden durchlauf um 1 nach Rechts geshifted
+    while(BIT >= 0x01)
     {
         GPIO_PORTK_DATA_R |= VOLT;
+        wait(300);                                                                               
 
-        wait(300);                                                                                 // SObald am Eingang des DAC das Signal geändert wird braucht es 30 mikrosekunden bis das Signal an PD(0) stabil anliegt.
-
-        if ((GPIO_PORTD_AHB_DATA_R & 0x1) == 0x01 )                                               // Sobald PD(1) und PD(0) auf high gesetzt sind kann der Wert auf die Variable VOLT gespeichert werden.
+        if ((GPIO_PORTD_AHB_DATA_R & 0x1) == 0x01 )
         {
         	BIT = BIT/2;
-        	VOLT |= BIT;                                                              // Wert wird gespeichert
+        	VOLT |= BIT;
         }
-        else if (GPIO_PORTD_AHB_DATA_R == 0x00)                                                   // Sollte PD(0) auf Low gesetzt sein wird der Wert nicht gespeichert und mit dem nächsten geshifteten werd gearbeitet
+        else if (GPIO_PORTD_AHB_DATA_R == 0x00)
         {
         	 VOLT &=(~BIT);
          	 GPIO_PORTK_DATA_R&=(~BIT);
         	 BIT = BIT/2;
         	 VOLT |= BIT;
-        	 continue;                                                                            // Zurück zur for schleife
+        	 continue;
         }
     }
     GPIO_PORTK_DATA_R &= 0x00;
+    return VOLT;
 }
-
-return VOLT;
-}
-
-/**
-
- * main.c
-
- */
 
 void main(void)
 {
-    float voltageStep  = 0.19;
+    float voltageStep = 0.19;
     unsigned short measuredVoltageDigit = 0;
     int measuredVoltage = 0;
-    int firstDigit = 0;
-    int changeDigit = 0;
-    int secondDigit = 0;
+    int firstDigit, secondDigit, thirdDigit, forthDigit = 0;
 
-    ConfigPorts();                                                                                   // Konfiguration der Ports
+    ConfigPorts();
 
     while (1)
     {
-        measuredVoltageDigit = (DACEXTERN() / 8);
-        measuredVoltage = (measuredVoltageDigit * voltageStep) * 100;
-        printf("%d\n", measuredVoltage);
+    	while(!(GPIO_PORTD_AHB_DATA_R & 0x02))
+    	{
+    		measuredVoltageDigit = (DACEXTERN() / 8);
+    		measuredVoltage = (measuredVoltageDigit * voltageStep) * 100;
+    		printf("%d\n", measuredVoltage);
+
+            firstDigit = measuredVoltage / 1000;
+            secondDigit = (measuredVoltage / 100) - (firstDigit * 10);
+            thirdDigit = (measuredVoltage / 10) - (secondDigit * 10) - (firstDigit * 100);
+            forthDigit = measuredVoltage - (thirdDigit * 10) - (secondDigit * 100) - (firstDigit * 1000);
+    	}
+        GPIO_PORTM_DATA_R |= forthDigit;
+        GPIO_PORTM_DATA_R |= (thirdDigit << 1);
+        GPIO_PORTM_DATA_R |= (secondDigit << 2);
+        GPIO_PORTM_DATA_R |= (firstDigit << 3);
     }
 }
+
 
